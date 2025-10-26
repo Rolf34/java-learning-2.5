a7c6109a218652b50e0a39d3923d479eb263e8d0
Livitsky Dmitry <139865238+Rolf34@users.noreply.github.com>
2025-10-27 02:08:29 +0300

    Update Task.java

--- krasava 4/src/company/projects/Task.java
+++ krasava 4/src/company/projects/Task.java
@@ -7,9 +7,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * Represents a task within a project.
- */
 public class Task extends ProjectEntity {
     public enum TaskPriority { LOW, MEDIUM, HIGH, CRITICAL }
 
